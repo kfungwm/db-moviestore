0b5adbe6bb8c0473d57ec9b0cb8c803968927fe8
Kevin Fung <kevinfung87@gmail.com>
2016-11-10 16:35:12 +0100

    updated the files

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express'),
       morgan = require('morgan'),
-      pug = require('pug');
+      bodyParser= require('body-parser'),
+      methodOverride = require('method-override'),
+      pug = require('pug'),
+      Sequelize = require('sequelize');
 
 var app = express();
+    sequelize = new Sequelize('Kevin', 'Kevin', '', { dialect: 'postgres' });
+
+var moviesRouter = require('./routes/movies');
+
+app.use(express.static('public'));
 
 app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use(methodOverride((req, res) => {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }})
+);
 
 app.set('view engine', 'pug');
 
@@ -12,14 +30,33 @@ app.get('/', (request, response) => {
   response.redirect('/movies');
 });
 
-app.get('/movies', (request, response) => {
-  response.render('./movies/index');
-});
+app.use('/movies', moviesRouter);
 
-app.get('/movies/new', (request, response) => {
-  response.render('./movies/new');
-});
 
-app.listen(3000, () => {
-  console.log('Web Server is running on port 3000');
+// app.get('/movies', (request, response) => {
+//   Movie.findAll().then((movies) => {
+//     response.render('movies/index', { movies: movies });
+//   });
+// });
+//
+// app.post('/movies', (request, response) => {
+//   Movie.create(request.body).then(() => {
+//       response.redirect('/movies');
+//   });
+// });
+//
+// app.get('/movies/new', (request, response) => {
+//   response.render('movies/new');
+// });
+//
+// app.get('movies/:id/edit', (request, response) => {
+//   Movie.findById(request.params.id).then((book) => {
+//     response.render('movies/edit', { movie: movie });
+//   });
+// });
+
+sequelize.sync().then(() => {
+  app.listen(3000, () => {
+    console.log('Web Server is running on port 3000');
+  });
 });
---
+++ routes/movies.js
@@ -0,0 +1,73 @@
+const express = require('express'),
+      morgan = require('morgan'),
+      bodyParser = require('body-parser'),
+      methodOverride = require('method-override'),
+      pug = require('pug'),
+      Sequelize = require('sequelize');
+      router = express.Router();
+
+const sequelize = new Sequelize('Kevin', 'Kevin', '', { dialect: 'postgres' });
+
+var Movie = sequelize.define('movie', {
+  title: Sequelize.STRING,
+  imageURL: Sequelize.STRING,
+  director: Sequelize.STRING,
+  description: Sequelize.TEXT
+});
+
+router.get('/', (request, response) => {
+  Movie.findAll({ order:'id ASC'}).then((movies) => {
+    response.render('movies/index', { movies: movies });
+  });
+});
+
+
+router.get('/new', (request, response) => {
+  response.render('movies/new');
+});
+
+router.get('/:id', (request, response) => {
+  Movie.findById(request.params.id).then((movie) => {
+    response.render('movies/show', { movie: movie });
+  });
+});
+
+router.get('/:id/edit', (request, response) => {
+  Movie.findById(request.params.id).then((movie) => {
+    response.render('movies/edit', { movie: movie });
+  });
+});
+
+router.post('/', (request, response) => {
+  if (request.body.title) {
+    Movie.create(request.body).then(() => {
+      response.redirect('/movies');
+    });
+  } else {
+    response.redirect('/movies/new');
+  }
+});
+
+
+router.put('/id', (request, response) => {
+  Movie.update(request.params, {
+    where: {
+      id: request.params.id
+    }
+  }).then(() => {
+    response.redirect('/movies/' + request.params.id);
+  });
+});
+
+router.delete('/:id', (request, response) => {
+  Movie.destroy({
+    where: {
+      id: request.params.id
+    }
+  }).then(() => {
+    response.redirect('/movies');
+  });
+});
+
+
+module.exports = router;
